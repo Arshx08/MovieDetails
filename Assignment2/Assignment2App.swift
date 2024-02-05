d0b21ede7cca3e9be045ff7417c7dfa7f348fd45
Arshdeep Singh <sing7434@sheridancollege.ca>
2024-02-05 11:47:51 -0500

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by Arshdeep Singh on 2024-02-05.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment2
+//
+//  Created by Arshdeep Singh on 2024-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
